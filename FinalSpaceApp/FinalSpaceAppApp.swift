7e4d8cd97494217d15d4d7a9c4ab3c11f25e5065
Popovich2005 <popich2005@mail.ru>
2024-07-13 18:15:48 +0300

    Initial Commit

---
+++ FinalSpaceApp/FinalSpaceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalSpaceAppApp.swift
+//  FinalSpaceApp
+//
+//  Created by Алексей Попов on 13.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FinalSpaceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
